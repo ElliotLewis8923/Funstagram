0ed9fd3de11bff00193f11a8358b6745c7b1171d
elliotlewis8923 <elewis8923@gmail.com>
2014-10-06 13:03:02 +0100

    passed second feature test; displays a form for new post

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -4,9 +4,16 @@ describe 'post' do
     context 'no posts submitted' do
         
         it 'should display a prompt to add a post' do
-            visit '/posts'
-            expect(page).to have_content 'No posts'
-            expect(page).to have_link 'Add a post'
+          visit '/posts'
+          expect(page).to have_content 'No posts'
+          expect(page).to have_link 'Add a post'
+        end
+
+        it 'should provide a form to submit a post' do
+        	visit '/posts'
+        	click_link 'Add a post'
+        	expect(page).to have_content 'Caption'
+        	expect(page).to have_content 'Image'
         end
     end
 end
