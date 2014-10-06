f687cdd39561e5f64f79350e9e50006518f9380c
elliotlewis8923 <elewis8923@gmail.com>
2014-10-06 21:19:37 +0100

    wrote and passed modal tests for comment regarding associations and validations
    Former-commit-id: 9fe8c121910597c79268705c0c5f63d769e0919c

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,8 @@ class Comment < ActiveRecord::Base
 
 	belongs_to :post
 	belongs_to :user
-	
+
+	validates :text, presence: true
+	validates :text, length: { maximum: 200,
+    too_long: "Your comment must contain at most %{count} characters" }
 end
