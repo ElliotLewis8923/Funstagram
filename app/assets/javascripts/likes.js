3efa02c10aa1f803577d8c00b98b7723250ea2de
ElliotLewis8923 <elewis8923@gmail.com>
2014-10-12 17:09:45 +0100

    finally got like unlike button working properly with ajax
    Former-commit-id: f0b3efdaa02065f36381322df5ecc5a983c851fd

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -3,20 +3,25 @@ $(document).ready(function(){
 
 	$('.like-link').on('click', function(event){
 		var likeCount = $(this).siblings('span');
-		var likeLink = $(this);
+		var likeLink = $(this).children('span');
 		$.post(this.href, function(response){
+			console.log(likeLink);
 			likeCount.text(response.updatedlikesCount);
-			likeLink.html(response.unlike);
+			console.log(response.link)
+			likeLink.text(response.link);
 		});	
 	});
 
-	$('.unlike-link').on('click', function(event){
-		console.log('clicked');
-		var likeCount = $(this).siblings('span');
-		var unlikeLink = $(this);
-		$.post(this.href, function(response){
-			likeCount.text(response.updatedlikesCount);
-			unlikeLink.text(response.like);
-		});
-	});
 });
+
+// 	$('.like-link').on('click', function(event){
+// 		console.log('clicked');
+// 		var likeCount = $(this).siblings('span');
+// 		var unlikeLink = $(this);
+// 		$.post(this.href, function(response){
+// 						console.log(response)
+// 			likeCount.text(response.updatedlikesCount);
+// 			unlikeLink.html(response.like);
+// 		});
+// 	});
+// });
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,20 +6,24 @@ class LikesController < ApplicationController
 		@post = Post.find(params[:post_id])
 		if @post.likes.find_by(:user_id => current_user.id).nil?
 			@like = @post.likes.create(:user_id => current_user.id)
-			render json: { updatedlikesCount: pluralize(@post.likes.count, 'like'), unlike: 
-				"<a class='unlike-link' data-method='delete' data-remote='true' href='/posts/" + @post.id.to_s + "/likes/" + @like.id.to_s + "' rel='nofollow'>Unlike</a>" }
+			render json: { updatedlikesCount: pluralize(@post.likes.count, 'like'), link: 
+				'Unlike' }
 		else
-			redirect_to post_path(@post)
+			@post.likes.find_by(:user_id => current_user.id).destroy
+			render json: { updatedlikesCount: pluralize(@post.likes.count, 'like'), link: 
+				'Like' }
 		end
 	end
 
-	def destroy
-		@post = Post.find(params[:post_id])
-		@like = Like.find(params[:id])
-		@like.destroy
-		render json: { updatedlikesCount: pluralize(@post.likes.count, 'like'), like: 
-			"<a class='like-link' data-method='post' data-remote='true' href='/posts/" + @post.id.to_s + "/likes' rel='nofollow'>Like</a>" }
-	end
+	# def destroy
+	# 	@post = Post.find(params[:post_id])
+	# 	@like = Like.find(params[:id])
+	# 	@like.destroy
+	# 	render json: { updatedlikesCount: pluralize(@post.likes.count, 'like'), like: 
+	# 		"<a class='like-link' data-method='post' data-remote='true' href='/posts/" + @post.id.to_s + "/likes' rel='nofollow'>Like</a>" }
+	# end
+
+	#<%= link_to 'Unlike', post_like_path(@post, @post.likes.find_by(:user_id => current_user.id)), method: :delete, remote: true, class: "unlike-link" %>
 
 
 
