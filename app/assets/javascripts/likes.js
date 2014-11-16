dd174b0d936b1e169dcad58c3a98b956f696756b
ElliotLewis8923 <elewis8923@gmail.com>
2014-11-16 10:24:56 +0000

    modal now hides correctly when closed
    Former-commit-id: 27f8495e5f1caaf525902b82e3778a5f46a4a199

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -4,9 +4,7 @@ $('#ajax-modal').on('click', 'a', function(e) {
 	var likeCount = $('section#like-links span');
 	var likeLink = $('section#like-links #link-name');
 	$.post(this.href, function(res) {
-		console.log('blah');
 		likeCount.text(res.updatedlikesCount);
-		console.log(res.link);
 		likeLink.html("<a class='like-link' data-method='post' data-remote='true' href='/posts/" + res.postId + "/likes' rel='nofollow'>" + res.link + "</a>");
 	});
 });
