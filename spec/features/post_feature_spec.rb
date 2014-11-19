c4d17b65039ca38911e8d14b6971e9c89f9f7b88
ElliotLewis8923 <elewis8923@gmail.com>
2014-11-19 15:24:46 +0000

    post test suite now passing
    Former-commit-id: 67d49ffa4cffe6de0722f0caac15e9d313434159

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -6,23 +6,21 @@ describe 'Posts' do
 
     	before(:each) do
     		login_as create(:elliot)
+            visit '/posts'
     	end
         
         it 'should display a prompt to add a post' do
-          visit '/posts'
           expect(page).to have_content 'No posts'
           expect(page).to have_link 'Add a post'
         end
 
         it 'should provide a form to submit a post' do
-        	visit '/posts'
         	click_link 'Add a post'
         	expect(page).to have_content 'Caption'
         	expect(page).to have_content 'Image'
         end
 
         it 'should submit a post' do
-        	visit '/posts'
         	click_link 'Add a post'
         	fill_in 'Caption', :with => 'first post'
         	attach_file('Image', './app/assets/images/testimage.png')
@@ -37,16 +35,15 @@ describe 'Posts' do
     	before(:each) do
     		@post = create(:post)
     		login_as create(:elliot)
+            visit '/posts'
     	end
 
     		it 'should be displayed with their images on the homepage' do
-    			visit '/posts'
-    			expect(page).to have_css("Testimage")
+    			expect(page).to have_css('.image-link')
     		end
 
     		it 'should be display as a larger image when clicked' do
-    			visit '/posts'
-    			find('Testimage').trigger('click')
+    			find('#masonry div:first-child .image-link').click
     			expect(page).to have_css("#ajax-modal")
     		end
 
