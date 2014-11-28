8d47b6ea9f674a4a4a76f4d45baab94f6f7ec2a4
ElliotLewis8923 <elewis8923@gmail.com>
2014-11-28 16:33:45 +0000

    adjusted indexe and users page css
    Former-commit-id: d2a7a7946da28e07399e349ecb8807d3840a86fc

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -2,10 +2,11 @@ $('#ajax-modal').on('click', '#like-link', function(e) {
 	e.stopPropagation();
 	e.preventDefault();
 	var likeCount = $('section#like-links span');
-	var likeLink = $('section#like-links #link-name');
+	var likeLink = $('#link-name');
 	$.post(this.href, function(res) {
+		console.log(res.link);
 		likeCount.text(res.updatedlikesCount);
-		likeLink.html("<a id='like-link' data-method='post' data-remote='true' href='/posts/" 
+		likeLink.html("<a id='like-link' data-method='post' data-remote='true' class='btn btn-default' href='/posts/" 
 			+ res.postId 
 			+ "/likes' rel='nofollow'>" 
 			+ res.link 
